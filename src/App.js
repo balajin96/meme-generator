03ccf964988eab19963cf5c6c40cf1dc7c7d892d
balajin96 <balajinatarajan18011996@gmail.com>
2022-06-19 23:26:06 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+import Header from "./Header.js";
+import Main from "./main.js"
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+        <Main />
+    </div>
+  );
+}
+
+export default App;
